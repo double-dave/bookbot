768602544887afae52ce25f2b8500041ff380aa3
dave <108686200+Ddavis461@users.noreply.github.com>
2025-06-05 19:03:00 -0400

    update book path and book selection

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from stats import count_words, get_chars_dict
+from stats import count_words, get_chars_dict, chars_dict_to_sorted_list
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    # book_path = "books/frankenstein.txt"
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    
+    # text = get_book_text(book_path)
+    book_path = sys.argv[1]
+    text = get_book_text(sys.argv[1])
     num_words = count_words(text)
     chars_dict = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
     
 
@@ -15,5 +24,19 @@ def get_book_text(filepath):
         return f.read()
 
 
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    
+    
+    print("============= END ===============")
 
 main()
